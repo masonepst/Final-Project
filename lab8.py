0d98f037cc9464f4922b673062e60978132330bf
masonepst <masonepst@gmail.com>
2025-12-10 20:32:05 -0500

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -95,17 +95,21 @@ if __name__ == '__main__':
     # Instantiate 2 Steppers:
     m1 = Stepper(s, lock1, newlock)
     m2 = Stepper(s, lock2, newlock)
-s
+
 
     m1.zero()
     m2.zero()
     m1.goAngle(90)
     delay(1000)
     m1.goAngle(-45)
+    delay(1000)
     m2.goAngle(-90)
     m2.goAngle(45)
+    delay(1000)
     m1.goAngle(-135)
+    delay(1000)
     m1.goAngle(135)
+    delay(1000)
     m1.goAngle(0)
  
     # While the motors are running in their separate processes, the main
