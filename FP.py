0032c94f88581b13afff844ce2d5f21c35714c72
masonepst <masonepst@gmail.com>
2025-12-15 18:13:58 -0500

    Add files via upload

--- FP.py
+++ FP.py
@@ -76,7 +76,14 @@ def Run():
             laser = "OFF"
             time.sleep(2)
 
-            motor1 = (180 - math.degrees(my_theta) + math.degrees(theta)) / 2
+            if my_theta>theta:
+
+                dif = my_theta - theta
+                motor1 = (180 - math.degrees(dif)) / 2
+            else:
+                dif = theta - my_theta
+                motor1 = -1 * (180 - math.degrees(dif)) / 2
+
             m1.goAngle(motor1)
 
             motor2 = 0
@@ -100,7 +107,14 @@ def Run():
         laser = "OFF"
         time.sleep(2)
 
-        motor1 = (180 - math.degrees(my_theta) + math.degrees(theta)) / 2
+        if my_theta>theta:
+
+                dif = my_theta - theta
+                motor1 = (180 - math.degrees(dif)) / 2
+            else:
+                dif = theta - my_theta
+                motor1 = -1 * (180 - math.degrees(dif)) / 2
+                
         length = 2 * my_r * math.cos(math.radians(motor1))
         motor2 = math.degrees(math.atan(dist_z / length))
 
